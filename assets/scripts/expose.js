825a0a80983bb779e437c2fbd743c6009abd8f62
wengthree1cm <103155845+wengthree1cm@users.noreply.github.com>
2023-05-03 16:45:38 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,7 +7,10 @@ function init() {
   // select a horn from drop down menu.
   const selectHorn = document.getElementById("horn-select");
   const hornImage = document.querySelector("#expose img");
-  const hornAudio = document.getElementsByClassName("hidden");
+  const hornAudio = new Audio();
+  const volume = document.querySelector("#volume-controls img");
+  const volumeslider = document.getElementById("volume");
+  const play=document.querySelector("#expose button");
 
   // handle drop down change event
   selectHorn.addEventListener("change", (event) => {
@@ -24,8 +27,30 @@ function init() {
     hornImage.src = hornImageRoute;
     hornImage.alt = selectedValue;
     hornAudio.src = hornAudioRoute;
+
   });
 
-  
+  volumeslider.addEventListener("change",(event)=>{
+    const selectedvolume=event.target.value;
+    hornAudio.volume=selectedvolume/100;
+    if(selectedvolume==="0"){
+      volume.src="assets/icons/volume-level-0.svg";
+    }
+    else if(selectedvolume<34){
+      volume.src="assets/icons/volume-level-1.svg";
+    }
+    else if(selectedvolume<67){
+      volume.src="assets/icons/volume-level-2.svg";
+    }
+    else{
+      volume.src="assets/icons/volume-level-3.svg";
+    }
+  })
 
+  play.addEventListener("click",(event)=>{
+    hornAudio.play();
+    if (selectHorn==="party-horn"){
+        confetti.start();
+    }
+  });
 }
